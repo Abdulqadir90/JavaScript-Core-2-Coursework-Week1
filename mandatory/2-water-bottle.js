2f2012e986cb9c24b3a4cf0e8ac8fea86cb9bc19
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-01-15 03:27:51 +0000

    Update 2-water-bottle.js
    water bottle completed

--- mandatory/2-water-bottle.js
+++ mandatory/2-water-bottle.js
@@ -52,7 +52,7 @@ let bottle = {
   },
   isEmpty: function () {
     // this function should return true if your bottle is empty;
-    return this.volume === 0;
+    return this.volume === 0; 
   },
 };
 
