5d873c078a1f696c0806233d409a3ecd86242da2
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-01-15 03:20:48 +0000

    recipes
    i "think" its completed

--- mandatory/5-recipes.js
+++ mandatory/5-recipes.js
@@ -24,4 +24,50 @@ You should write and log at least 5 recipes
 
 **/
 
-let recipes = {};
+let recipes = [
+
+        {
+          title: "Vegetable Soup",
+          servings: 7,
+          ingredients: ["water", "onions", "carrots", "potatoes", "garlic", "green beans", "celery"],
+        },
+        {
+          title: "Coffee",
+          servings: 3,
+          ingredients: ["water", "coffee beans", "milk"],
+        },
+        {
+          title: "Pizza",
+          servings: 6,
+          ingredients: ["dough", "tomato sauce","cheese", "chicken", "pineapple toppings", "barbecue sauce"],
+        },
+        {
+          title: "ice cream",
+          servings: 4,
+          ingredients: ["milk protein", "ice", "water", "sugar"],
+        },
+        {
+          title: "Tea",
+          servings: 3,
+          ingredients: ["teabag", "water", "milk"],
+        },
+        ];
+
+        let food = recipes.forEach((value) => {
+            console.log(`${value.title} ${value.servings} ${value.ingredients}`);
+        })
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
