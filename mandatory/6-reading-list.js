39a351701b9c029ed7e7c2b2efd37255ec460f9c
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-01-15 19:30:30 +0000

    reading list 1 completed exercise 2 not completed yet

--- mandatory/6-reading-list.js
+++ mandatory/6-reading-list.js
@@ -21,6 +21,68 @@ Now use an if/else statement to change the output depending on whether you have 
 If you've read it, log a string like 'You've already read "The Hobbit" by J.R.R. Tolkien', 
 and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
 
-**/
+**/ 
+// Exercise 1
 
-let books = [];
+let books = [
+
+    {
+        Title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        Reading: ["false"],
+      },
+      {
+        Title: "The Compound Effect",
+        author: "Darren Hardy",
+        Reading: ["true"],
+      },
+      {
+        Title: "Think and Grow Rich",
+        author: "Napoleon Hill",
+        Reading: ["false"],
+      },
+      {
+        Title: "How To Win Friends And Influence People",
+        author: "Dale Carnegie",
+        Reading: ["false"],
+      },
+      {
+        Title: "The Power Of Habit",
+        author: "Charles Duhigg",
+        reading: ["true"],
+      },
+
+    ];
+
+      let n = books.forEach((value) => {
+        console.log(`${value.Title} ${value.author}`);
+      });
+
+      // Exercise 2
+
+      let nn = books.forEach((value) => {
+        if(value.reading === true){
+          console.log(`you have already read ${value.title} by ${value.author}`);
+        } else {
+          console.log(`you still need to read ${value.title} by ${value.author}`);
+        }
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
